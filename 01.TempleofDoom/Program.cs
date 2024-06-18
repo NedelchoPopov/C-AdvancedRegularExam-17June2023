0a45f3fd7ec8a73d166321eb3dc791b660304ef9
Nedelcho <160885648+NedelchoPopov@users.noreply.github.com>
2024-06-18 21:56:24 +0300

    Add project files.

---
+++ 01.TempleofDoom/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+2 4 6 8
+11 3 5 7 9
+24 28 18 30
+
+13 7 4 22 11 15 20
+3 2 1
+12 10 5
+
+*/
+
+Queue<int> tools = new Queue<int> (Console.ReadLine()
+    .Split()
+    .Select(int.Parse));
+
+Stack<int> substances = new Stack<int>(Console.ReadLine()
+    .Split()
+    .Select(int.Parse));
+
+List<int> challenges = new List<int>(Console.ReadLine()
+    .Split()
+    .Select(int.Parse));
+
+while (tools.Count > 0 && substances.Count > 0)
+{
+    int currentTool = tools.Dequeue();
+    int currentSubstance = substances.Pop();
+    int result = currentTool * currentSubstance;
+    var resultIsEqual = challenges.FirstOrDefault(x => x.Equals(result));
+
+    if (resultIsEqual != default)
+    {
+        challenges.Remove(resultIsEqual);
+    }
+    else
+    {
+        tools.Enqueue(++currentTool);
+        currentSubstance--;
+        if (currentSubstance == 0)
+        {
+            continue;
+        }
+        substances.Push(currentSubstance);
+    }
+
+    if (challenges.Count == 0)
+    {
+        break;
+    }
+}
+
+if (challenges.Count == 0)
+{
+    Console.WriteLine("Harry found an ostracon, which is dated to the 6th century BCE.");    
+}
+else if (tools.Count == 0 || substances.Count == 0)
+{
+    Console.WriteLine("Harry is lost in the temple. Oblivion awaits him.");
+
+}
+
+PrintSequences();
+void PrintSequences()
+{
+    if (tools.Any())
+    {
+        Console.WriteLine($"Tools: {string.Join(", ", tools)}");
+    }
+    if (substances.Any())
+    {
+        Console.WriteLine($"Substances: {string.Join(", ", substances)}");
+    }
+    if (challenges.Any())
+    {
+        Console.WriteLine($"Challenges: {string.Join(", ", challenges)}");
+    }
+}
---
+++ 02.Mouse InTheKitchen/Program.cs
@@ -0,0 +1,109 @@
+﻿string[] size = Console.ReadLine().Split(',');
+int rows = int.Parse(size[0]);
+int cols  = int.Parse(size[1]);
+int mauseRow = 0;
+int mauseCol = 0;
+int cheeseCount = 0;
+char[,] cupboard = new char[rows, cols];
+
+for (int row = 0; row < cupboard.GetLength(0); row++)
+{
+    string input = Console.ReadLine();
+
+    for (int col = 0; col < cupboard.GetLength(1); col++)
+    {
+        cupboard[row, col] = input[col];
+        if (input[col] == 'M')
+        {
+            mauseRow = row;
+            mauseCol = col;
+        }
+        if (input[col] == 'C')
+        {
+            cheeseCount++;
+        }
+    }
+}
+ 
+ string command;
+while((command = Console.ReadLine()) != "danger")
+{
+    int nextRow = mauseRow;
+    int nextCol = mauseCol;
+    switch (command)
+    {
+        case "up":
+            nextRow--;
+            break;
+        case "down":
+            nextRow++;
+            break;
+        case "right":
+            nextCol++;
+            break;
+        case "left":
+            nextCol--;
+            break;
+    }
+    if (!IsInsideBoard(cupboard, nextRow, nextCol))
+    {
+        Console.WriteLine("No more cheese for tonight!");
+        PrintMetrix();
+        return;
+    }
+    if (cupboard[nextRow, nextCol] == '@')
+    {
+        continue;
+    }
+
+
+    cupboard[mauseRow, mauseCol] = '*';
+    mauseRow = nextRow;
+    mauseCol = nextCol;
+    if (cupboard[nextRow, nextCol] == '*')
+    {
+        cupboard[nextRow, nextCol] = 'M';
+    }
+    if (cupboard[nextRow,nextCol] == 'C')
+    {
+        cheeseCount--;
+        cupboard[nextRow, nextCol] = 'M';
+        if (cheeseCount == 0)
+        {
+            Console.WriteLine("Happy mouse! All the cheese is eaten, good night!");
+            break;
+        }
+    }
+    else if (cupboard[nextRow, nextCol] == 'T')
+    {
+        cupboard[nextRow, nextCol] = 'M';
+        Console.WriteLine("Mouse is trapped!");
+        PrintMetrix();
+        return;
+    }
+}
+if (cheeseCount != 0)
+{
+    Console.WriteLine("Mouse will come back later!");
+}
+
+PrintMetrix();
+void PrintMetrix()
+{
+    for (int row = 0; row < cupboard.GetLength(0); row++)
+    {
+        for (int col = 0; col < cupboard.GetLength(1); col++)
+        {
+            Console.Write(cupboard[row, col]);
+        }
+        Console.WriteLine();
+    }
+
+}
+
+bool IsInsideBoard(char[,] board, int row, int col)
+{
+
+    return row >= 0 && row <= board.GetLength(0) - 1
+        && col >= 0 && col <= board.GetLength(1) - 1;
+}
